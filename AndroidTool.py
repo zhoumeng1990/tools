f79411710b8e28ad65f84b4dfaa99e71fb1bafba
zhoumeng1990 <596878238@qq.com>
2019-08-23 20:05:09 +0800

    Android 解析部分代码完善

--- AndroidTool.py
+++ AndroidTool.py
@@ -6,11 +6,11 @@ import xml.dom.minidom as XmlDocument
 
 dir_path = os.path.abspath('.') + '/'
 manifest_path = dir_path + '/app/AndroidManifest.xml'  # AndroidManifest路径
-apk_path = dir_path + '/app.apk'  # apk路径
+app_name = ''
 
 def readInfo():
     # 使用minidom解析器打开 XML 文档
-    DOMTree = XmlDocument.parse(manifest_path)
+    DOMTree = XmlDocument.parse(dir_path + '/' + app_name + '/AndroidManifest.xml')
     manifestNode = DOMTree.getElementsByTagName('manifest')[0]
     print 'packageName: '+manifestNode.getAttribute('package')
     collection = DOMTree.documentElement
@@ -34,11 +34,12 @@ if __name__ == '__main__':
 
     files = os.listdir(dir_path)
     for file in files:
-        if os.path.splitext(file)[0] == 'app' and os.path.splitext(file)[-1][1:] == 'apk':
-            if os.path.exists(dir_path + 'app'):
-                shutil.rmtree(dir_path + 'app')
+        if os.path.splitext(file)[-1][1:] == 'apk':
+            app_name = os.path.splitext(file)[0]
+            if os.path.exists(dir_path + app_name):
+                shutil.rmtree(dir_path + app_name)
             # 反编译
-            decode_cmd = 'apktool d app.apk'
+            decode_cmd = 'apktool d ' + app_name + '.apk'
             os.system('cd ' + dir_path)
             os.system(decode_cmd)
             readInfo()
